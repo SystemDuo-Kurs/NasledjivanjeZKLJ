90a0cf45b0220f8d019858c62942aa8cf3b89114
Dragan Pepic <d.pepic@hotmail.com>
2022-02-03 18:01:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Object o = new();
+
+Osoba osoba = new();
+Druga d = new();
+
+internal class Osoba
+{
+    public string Nesto;
+
+    public void Foo()
+    { }
+}
+
+internal class Druga : Osoba
+{
+    public int X;
+}
